b6f04f8a1fbdb6e4c543526b8c9ce430be4a9e0e
mostafa bukhadra <mvxizk@gmail.com>
2023-11-29 04:12:30 +0200

    ok

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,15 +1,45 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
+listint_t *add(int number)
+{
+		listint_t *new_node = malloc(sizeof(listint_t));
+		
+		if(!new_node)
+			return (NULL);
+		new_node->n = number;
+		new_node->next = NULL;
+		return (new_node);
+}
+listint_t *check_first(listint_t **head, int number)
+{
+		listint_t *new_node = malloc(sizeof(listint_t));
+		listint_t *temp = *(head);
+
+		if (!new_node)
+			return (NULL);
+		new_node->n = number;
+		if (number >= temp->n)
+		{
+			new_node->next = NULL;
+			temp->next = new_node;
+		}
+		else
+		{
+			new_node->next = temp;
+			temp = new_node;
+		}
+		return (temp);
+}
 listint_t *insert_node(listint_t **head, int number)
 {
 		listint_t *temp = *(head);
 		listint_t *new_node = NULL;
 
 		if (!head)
-			add_nodeint_end(head, number);
-		else
-		{
+			return add(number);
+		if (temp->next == NULL || temp->n <= number)
+			return (check_first(head, number));
 		while(temp->next->n <  number && temp->next != NULL)
 			temp = temp->next;
 		new_node = malloc(sizeof(listint_t));
@@ -18,6 +48,5 @@ listint_t *insert_node(listint_t **head, int number)
 		new_node->n = number;
 		new_node->next = temp->next;
 		temp->next = new_node;
-		}
 		return (new_node);
 }
