64faee29a543bbd89d391e0986d23a5c2d84b5b8
mostafa bukhadra <mvxizk@gmail.com>
2023-11-30 23:34:41 +0200

    ok

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for i in range(65, 98):
-        print(isupper(chr(i)))
+    for i in range(65, 91):
+        char = chr(i)
+        print(char.upper(), end='')
