7cce3a8e3010bcb97f227f68f54443f349d1570b
mostafa bukhadra <mvxizk@gmail.com>
2023-11-30 23:57:39 +0200

    ok

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     if sys.argv[2] == '+' or sys.argv[2] == '-' or sys.argv[2] == '/' or sys.argv[2] == '*':
         state = True
     if state:
@@ -22,6 +22,5 @@ if __name__ == "__main__":
         else:
             print("{} / {} = {}" .format(a, b, calc.div(a, b)))
     else:
-        print(sys.argv[2])
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
