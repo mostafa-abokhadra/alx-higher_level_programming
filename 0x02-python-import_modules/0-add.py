c187add3ba1becf1ef873f9ba1622fda60096566
mostafa bukhadra <mvxizk@gmail.com>
2023-11-30 22:25:55 +0200

    ok

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add_file
 if __name__ == "__main__":
+    import add_0 as add_file
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add_file.add(a, b)))
