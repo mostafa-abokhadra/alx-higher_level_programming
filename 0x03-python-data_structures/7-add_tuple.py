732b663b029afde1f7a66be8f25be738c2a87a77
mostafa bukhadra <mvxizk@gmail.com>
2023-12-02 07:17:01 +0200

    ok

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 0
+    b = 0
+    ll = []
+    if len(tuple_a) < 2:
+        for i in tuple_a:
+            ll.append(i)
+        for i in range(len(ll), 2):
+            ll.append(0)
+        tuple_a = tuple(ll)
+
+    if len(tuple_b) < 2:
+        for i in tuple_b:
+            ll.append(i)
+        for i in range(len(ll), 2):
+            ll.append(0)
+        tuple_b = tuple(ll)
+
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
+    newtup = (a, b)
+    return newtup
