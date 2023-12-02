bfc3d1a3720d3aa82226578565ed31845bd83996
mostafa bukhadra <mvxizk@gmail.com>
2023-12-02 18:04:04 +0200

    ok

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    char = sentence[0] if total > 0 else "None"
+    char = sentence[0] if length > 0 else "None"
     tub = length, char
     return(tub)
