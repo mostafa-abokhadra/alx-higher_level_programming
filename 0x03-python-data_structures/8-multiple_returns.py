42a54ab926a6b2e6d6b72c12120d8861c1a7974a
mostafa bukhadra <mvxizk@gmail.com>
2023-12-02 18:03:02 +0200

    ok

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence[0] = None
-    Tuple = (len(sentence), sentence[0])
-    return Tuple
+    length = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    tub = total, char
+    return(tub)
