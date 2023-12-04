2441dca9e9ca4e21d0385a7b0862dc137598bf0d
mostafa bukhadra <mvxizk@gmail.com>
2023-12-04 19:37:44 +0200

    ok

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,40 @@
-#include "list.h"
+#include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
 int is_palindrome(listint_t **head)
 {
-		listint_t *h = *(head), *last = NULL;
-		int pal = 1;
+		listint_t *h = *(head);
+		int pal = 1, size = 0;
+		int *rev = NULL;
 
-		if (!head || !(*(head)->next))
+		if (!head || !((*head)->next))
 			return (1);
-		while (h->next != NULL)
-			h = h->next;
-		last = h;
-		h = *(head);
-		while(h != last || last > h)
+		while (h != NULL)
 		{
-			if (h->data == last->data)
-				continue;
+			rev = realloc(rev, sizeof(int) * (++size));
+			if (!rev)
+				return (0);
+			if (h->next == NULL)
+			{
+				rev[size - 1] = h->n;
+				h->next = *(head);
+				h = h->next;
+				for (;h->next != *(head); h = h->next)
+				{
+					if (h->n == rev[--size])
+						continue;
+					pal = 0;
+				}
+				if (h->n != rev[--size])
+					pal = 0;
+				h->next = NULL;
+				break;
+			}
+			rev[size - 1] = h->n;
 			h = h->next;
-			last = 
-			pal = 0;
 		}
-		return (pal);
+		free(rev);
+		return pal;
 }
