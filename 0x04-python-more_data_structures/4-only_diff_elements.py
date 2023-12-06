8f64281b226468a1d1e9110f9ad63254cd3b8e45
mostafa bukhadra <mvxizk@gmail.com>
2023-12-06 12:27:55 +0200

    ok

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     # first
-    """new_list = []
+    new_list = []
     new_list = [i for i in set_1 if i not in set_2]
     new_list += [i for i in set_2 if i not in set_1]
     return set(new_list)
-    """
-    new_list = []
+    # c style
+    """new_list = []
     new_list = func(set_1, set_2)
     new_list += func(set_2, set_1)
     return set(new_list)
@@ -21,3 +21,4 @@ def func(set_1, set_2):
         if check:
             new_list.append(i)
     return new_list
+    """
