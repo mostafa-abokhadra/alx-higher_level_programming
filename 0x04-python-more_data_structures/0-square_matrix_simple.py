caec01f991d1433446a64873c796f84ab3202cb0
mostafa bukhadra <mvxizk@gmail.com>
2023-12-06 10:27:49 +0200

    ok

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,11 @@
 #!/bin/usr/python3
 def square_matrix_simple(matrix=[]):
-    new_list = []
+    new_mat = []
+    for i in range(len(matrix)):
+        a_list = map(lambda arg: arg**2, matrix[i])
+        new_mat.append(list(a_list))
+    return new_mat
+    """new_list = []
     new_mat = []
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
@@ -8,3 +13,4 @@ def square_matrix_simple(matrix=[]):
         new_mat.append(list(new_list))
         new_list.clear()
     return new_mat
+    """
