dc9fdf0c2099b0babf46ed5d8fca1396450c5c04
mostafa bukhadra <mvxizk@gmail.com>
2023-12-06 12:05:06 +0200

    ok

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -10,5 +10,6 @@ def common_elements(set_1, set_2):
     """listy = [i for i in set_1 if i in set_2]
     return set(listy)
     """
+    # using some complicated stuff
     new_list = list(filter(lambda arg:  arg in set_2, set_1))
     return new_list
