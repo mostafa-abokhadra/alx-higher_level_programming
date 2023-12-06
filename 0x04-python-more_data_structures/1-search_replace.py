298a751069b1298263d52d450546398cf10e5d58
mostafa bukhadra <mvxizk@gmail.com>
2023-12-06 10:40:44 +0200

    ok

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
+    # first method of solving succeded
+    """new_list = my_list[:]
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
+    return new_list
+    """
+    new_list = [i if i != search else replace for i in my_list]
     return new_list
