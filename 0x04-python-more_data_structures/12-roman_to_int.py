b7ac8ba9376bfd37f9624efeabff61e7484a0e22
mostafa bukhadra <mvxizk@gmail.com>
2023-12-06 16:00:57 +0200

    ok

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'n': 0}
     sumy = 0
-    if roman_string == None or not(isinstance(roman_string,str)):
+    if roman_string == None:
         return None
     if isinstance(roman_string, str):
         for i in range(len(roman_string)):
@@ -10,4 +10,4 @@ def roman_to_int(roman_string):
         for i in range(1, len(roman_string)):
             if roman[roman_string[i]] > roman[roman_string[i - 1]]:
                 sumy = sumy - roman[roman_string[i - 1]]
-    return sumy
+        return sumy
