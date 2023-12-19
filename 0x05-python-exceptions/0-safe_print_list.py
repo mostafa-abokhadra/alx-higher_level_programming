16f199bca0f363604d40bdb69d9ade1b58911314
mostafa bukhadra <mvxizk@gmail.com>
2023-12-19 03:07:50 +0200

    ok

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    it = 0
+    while it < x:
+        try:
+            print("{}".format(my_list[it]), end="")
+            num = num + 1
+        except:
+            break;
+        it = it + 1
+    print()
+    return num
