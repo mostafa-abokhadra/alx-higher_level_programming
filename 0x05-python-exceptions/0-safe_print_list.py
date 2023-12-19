6a2cd76bfc4b5b9d6cad723262c55537d30ec755
mostafa bukhadra <mvxizk@gmail.com>
2023-12-19 03:19:34 +0200

    ok

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[it]), end="")
             num += 1
         except:
-            continue
+            break
         it += 1
-    print()
+    print("")
     return num
