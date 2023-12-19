4b0244ea08df47a05a147ba6a61eea4b8c0d93a3
mostafa bukhadra <mvxizk@gmail.com>
2023-12-19 04:15:00 +0200

    ok

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,4 +18,6 @@ def list_division(my_list_1, my_list_2, list_length):
             listy.append(0)
             print("out of range")
         finally:
-            return listy
+            pass
+
+    return listy
