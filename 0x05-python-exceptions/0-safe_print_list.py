81c9fafd81abe0e0eec3191552ef652165ad8434
mostafa bukhadra <mvxizk@gmail.com>
2023-12-19 03:14:16 +0200

    ok

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,9 +7,9 @@ def safe_print_list(my_list=[], x=0):
     while it < x:
         try:
             print("{}".format(my_list[it]), end="")
-            num = num + 1
+            num += 1
         except:
             break;
-        it = it + 1
+        it += 1
     print()
     return num
