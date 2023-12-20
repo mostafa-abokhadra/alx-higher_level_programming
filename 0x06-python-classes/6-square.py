80f53b807d4488d211a8788acf81226ec0579471
mostafa bukhadra <mvxizk@gmail.com>
2023-12-20 05:23:00 +0200

    ok

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,9 +51,11 @@ class Square:
             for j in range(0, self.__size):
                 for k in range(0, self.__position[0]):
                     print(" ", end="")
-                for l in range(0, self.__size):
+                print("#" * (self.__size))
+                """for l in range(0, self.__size):
                     print("#", end="")
                 print()
+                """
     @property
     def position(self):
         """returns: the position
