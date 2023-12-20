3b52f131ea925e6b5c0625d3ba4ceb4252337728
mostafa bukhadra <mvxizk@gmail.com>
2023-12-20 03:41:05 +0200

    ok

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" """
+
+
+class Square:
+    """
+    """
+    def __inti__(self, size=0):
+        """
+        """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
