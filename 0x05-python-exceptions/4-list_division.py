262adc4a0620a388055deb01740f480414c970d3
mostafa bukhadra <mvxizk@gmail.com>
2024-01-14 00:38:06 +0200

    ok

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,6 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             div_list.append(0)
         except IndexError:
+            div_list.append(0)
             print("out of range")
         finally:
             pass
