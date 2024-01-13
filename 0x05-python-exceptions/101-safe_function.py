55073d8e0a9bf57d25d2ad1146da064dcdf7c39c
mostafa bukhadra <mvxizk@gmail.com>
2024-01-14 00:57:14 +0200

    ok

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res 
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return None
