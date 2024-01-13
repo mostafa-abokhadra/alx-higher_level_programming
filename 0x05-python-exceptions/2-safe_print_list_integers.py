0ce497ab2dd16325142937d4ebb837854d86bbf5
mostafa bukhadra <mvxizk@gmail.com>
2024-01-13 23:38:18 +0200

    ok

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except Exception as err:
             raise err
-    if not x == 0:
-        print()
+    print()
     return count
