5478bebf32913a53c82930d06ea35b8544dcbf5b
mostafa bukhadra <mvxizk@gmail.com>
2024-01-13 23:35:08 +0200

    ok

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    while count < x:
+        try:
+            if isinstance(my_list[count], int):
+                print("{:d}".format(my_list[count]), end="")
+            count += 1
+        except Exception as err:
+            raise err
+    print()
+    return count
