312fda8fcc84f806917320fe0cc787603732bf83
mostafa bukhadra <mvxizk@gmail.com>
2024-01-16 21:47:01 +0200

    ok

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,113 +1,42 @@
 #!/usr/bin/python3
-
-""" square class """
-
-
 class Square:
-    """ square class, op overloading """
-
+    """Defines a square"""
     def __init__(self, size=0):
-        """ initializing size
+        """Initialises the data"""
+        self.size = size
 
-        args:
-            size: size of square
-        """
-        self.__size = size
-        self.__area = size ** 2
-
+    def area(self):
+        """Returns current square area"""
+        return self.__size**2
 
     @property
     def size(self):
-        """ Return: the size of sq
-        """
+        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ setting the size
-
-        args:
-            value: value of size
-        """
-        if not isinstance(value, int) or not isinstance(value, float):
-            raise TypeError("size must be a number")
-        elif value < 0:
+        """Setter method"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.size = value
 
-    @property
-    def area(self):
-        """ Return:the area of square
-        """
-        return self.size ** 2
+    def __lt__(self, other):
+        return self.area() < other.area()
 
-    def __eq__(self, sq):
-        """ implementing equal equal op
+    def __le__(self, other):
+        return self.area() <= other.area()
 
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if self.area == sq.area:
-            return True
-        else:
-            return False
-    def __ne__(self, sq):
-        """ implementing not equal op
+    def __eq__(self, other):
+        return self.area() == other.area()
 
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if not self.area == sq.area:
-            return True
-        else:
-            return False
+    def __ne__(self, other):
+        return self.area() != other.area()
 
-    def __lt__(self, sq):
-        """ implementing less than op
+    def __gt__(self, other):
+        return self.area() > other.area()
 
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if self.area < sq.area:
-            return True
-        else:
-            return False
-
-    def __gt__(self, sq):
-        """ implementing greater than op
-
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if self.area > sq.area:
-            return True
-        else:
-            return False
-
-    def __le__(self, sq):
-        """ implementing less than or equal op
-
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if self.area <= sq.area:
-            return True
-        else:
-            return False
-
-    def __ge__(self, sq):
-        """ implementing greater than or equal op
-
-        args:
-            sq1: Square(first square)
-            sq2: Square(second square)
-        """
-        if self.area >= sq.area:
-            return True
-        else:
-            return False
+    def __ge__(self, other):
+        return self.area() >= other.area()
