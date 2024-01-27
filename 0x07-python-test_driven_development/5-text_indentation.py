e27a9b4aff9607c156f60386794a4754cc4dfd7f
mostafa bukhadra <mvxizk@gmail.com>
2024-01-27 19:45:19 +0200

    ok

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    final_str = ""
+    idx = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    while idx < len(text):
+        final_str += text[idx]
+        if text[idx] == '.' or text[idx] == '?' or text[idx] == ':':
+            final_str += '\n' * 2
+            idx = idx + 1
+        idx = idx + 1
+    print(final_str)
