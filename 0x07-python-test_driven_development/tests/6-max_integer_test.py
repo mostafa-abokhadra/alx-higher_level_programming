63d2d659cacc4a969e0f8b184e8af3a16658764c
mostafa bukhadra <mvxizk@gmail.com>
2024-01-31 01:17:47 +0200

    ok

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """test class of the module"""
 
-    def test_max(self):
+    def test_int(self):
         """testing nums 0, greater than zero and lesser"""
         self.assertAlmostEqual(max_integer([1, 2, 3, 4, 10]), 10)
         self.assertAlmostEqual(max_integer([-100, -200, -1]), -1)
@@ -19,6 +19,18 @@ class TestMaxInteger(unittest.TestCase):
     def test_str(self):
         """testing strings"""
         self.assertAlmostEqual(max_integer(['a', 'b', 'c']), 'c')
+        self.assertAlmostEqual(max_integer(["messi", "maradona", "ronaldinho"]), "ronaldinho")
+
+    def test_float(self):
+        """testing float"""
+        self.assertAlmostEqual(max_integer([2.5, 66.7, 1.4]), 66.7)
+
+    def test_lists(self):
+        """testing lists"""
+        self.assertAlmostEqual(max_integer([["messi"], ["messii"]]), ['messii'])
+        self.assertAlmostEqual(max_integer([("messi", 10), ("messi", 19)]), ('messi', 19))
+
+
 
     def test_empty(self):
         """testing empty list"""
