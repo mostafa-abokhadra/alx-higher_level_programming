73a213871445ff58ba38de236bc69bafed2782cf
mostafa bukhadra <mvxizk@gmail.com>
2024-01-30 20:07:34 +0200

    ok

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+
+print(matrix_mul(
+                [[1, 2, 3],
+                 [4, 5, 6]],
+                [[7, 8],
+                 [9, 10],
+                 [11, 12] ]))
