8baad9e3457e7bf81658a1440a655076054110c6
mostafa bukhadra <mvxizk@gmail.com>
2024-01-30 19:57:57 +0200

    ok

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+def assign_value(n, v):
+    n = v
+    print(n, v)
+    print(id(n))
+    print(id(v))
+
 l1 = [1, 2, 3]
-l2 = l1
-print(id(l1))
-print(id(l2))
-l1 = l1 + [4]
+l2 = [4, 5, 6]
+assign_value(l1, l2)
 print(id(l1))
 print(id(l2))
 print(l1)
-print(l2)
