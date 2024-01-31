342dce88e6c03b3eba117498de1e6d3701f60ec7
mostafa bukhadra <mvxizk@gmail.com>
2024-02-01 01:08:22 +0200

    done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -40,7 +40,6 @@ class Rectangle:
             raise ValueError('height must be >= 0')
         self.__height = value
 
-    @property
     def area(self):
         return self.width * self.height
 
