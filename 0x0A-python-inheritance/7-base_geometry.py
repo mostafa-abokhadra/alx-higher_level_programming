9cfecf77c50c572b07d8bd764142476a86832c1b
mostafa bukhadra <mvxizk@gmail.com>
2024-02-05 23:57:48 +0200

    done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module of gometry """
+
+
+class BaseGeometry:
+    """ class that not define an area :) """
+    def area(self):
+        """raising an exception of not defined area
+        """
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        """integer validate
+
+        args:
+            name: value key
+            value: value of key
+        """
+        if type(name) is not str:
+            raise TypeError("name must be a string")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
