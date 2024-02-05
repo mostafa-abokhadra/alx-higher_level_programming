ce93d29c6417991634a445a8a61ab599cdcd3128
mostafa bukhadra <mvxizk@gmail.com>
2024-02-05 23:05:06 +0200

    done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """ my list class
     """
-    def __init__(self):
-        """initailization of variables
-        """
     def print_sorted(self):
         """printing a sorted list """
         print(sorted(self))
