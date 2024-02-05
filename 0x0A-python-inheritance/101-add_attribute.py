d7246f3272f084f7d0fbf29b2707858d13604c62
mostafa bukhadra <mvxizk@gmail.com>
2024-02-06 01:39:26 +0200

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,12 @@
 
 
 def add_attribute(obj, name, value):
+    """adding attribute to object
+
+    args:
+        name: name of it
+        value: value of it
+    """
     if type(obj).__class__.__module__ == '__builtin':
         raise TypeError("can't add new attribute")
     obj.name = value
