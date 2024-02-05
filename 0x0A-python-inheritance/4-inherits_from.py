13598a52443428bada06259a2ac042166fd8c3b7
mostafa bukhadra <mvxizk@gmail.com>
2024-02-05 23:43:01 +0200

    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,6 +10,6 @@ def inherits_from(obj, a_class):
         obj: variable
         a_class: calss to be checked
     """
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and not type(obj) == a_class:
         return True
     return False
