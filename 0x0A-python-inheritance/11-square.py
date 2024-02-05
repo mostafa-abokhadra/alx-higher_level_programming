6b2b0a383ee59877833be2913da76a8685afe015
mostafa bukhadra <mvxizk@gmail.com>
2024-02-06 01:12:54 +0200

    done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" square module """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """initialize the size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """square area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """str function"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
