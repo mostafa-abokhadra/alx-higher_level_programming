6b6e320be4631ffd2ee998a0288e6fc42a244809
mostafa bukhadra <mvxizk@gmail.com>
2024-02-06 00:10:55 +0200

    done

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -3,13 +3,13 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", 12)
+bg.integer_validator("leo", 10)
 bg.integer_validator("width", 89)
 
 try:
-    bg.integer_validator("name", "John")
+    bg.integer_validator("name", 0)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print(e)
 
 try:
     bg.integer_validator("age", 0)
