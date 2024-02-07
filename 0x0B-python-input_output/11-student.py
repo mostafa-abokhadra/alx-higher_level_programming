58be931d913da00574e2231fd1073c8431582597
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 18:44:35 +0200

    done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""class module of student"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """initializing attrs
+
+        args:
+            first_name: 1st name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json function
+
+        args:
+            attrs: list of string
+        """
+        final_dic = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        for i in range(len(attrs)):
+            if attrs[i] in self.__dict__:
+                final_dic[attrs[i]] = self.__dict__[attrs[i]]
+        return final_dic
+
+    def reload_from_json(self, json):
+        """reload from json
+
+        args:
+            json: a dictionary
+        """
+        self.__dict__ = json
+
