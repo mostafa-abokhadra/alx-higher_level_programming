e2cbec69dfc14b150fab1b9306243f7762680087
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 15:13:40 +0200

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""write to file module"""
+
 
 def write_file(filename="", text=""):
+    """writing to file
+
+    args:
+        filename: pointer to a file to write in
+        text: text to write into the file
+    """
     with open(filename, mode='w', encoding='utf-8') as fily:
         return fily.write(text)
