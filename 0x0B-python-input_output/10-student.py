93dd6fdd5899192cec7b6d2c907741f1d422a913
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 18:27:46 +0200

    done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""student class module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """initializing attributes
+
+        args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    
+    def to_json(self, attrs=None):
+        """retrieves dictionary of an object"""
+        final_dic = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        for i in range(len(attrs)):
+            if attrs[i] in self.__dict__:
+                final_dic[attrs[i]] = self.__dict__[attrs[i]]
+        return final_dic
