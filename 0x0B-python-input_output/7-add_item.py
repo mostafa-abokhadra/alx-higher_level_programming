2250baeeca812a90d0d1ce757184b062238b1888
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 17:06:29 +0200

    done

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""load add and save module"""
+import json
+import sys
+import os
+
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
+
+listy = []
+
+if os.path.exists("add_item.json"):
+    listy = load_from("add_item.json")
+for i in range(1, len(sys.argv)):
+    listy.append(sys.argv[i])
+save_to(listy, "add_item.json")
