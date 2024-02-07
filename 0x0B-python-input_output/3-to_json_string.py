beb02fdab848fc3b7398476c87505045d3605f4e
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 15:20:19 +0200

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """convert to json module"""
-
 import json
 
-def to_json_string(my_obj):
-    """to json
 
-    args:
-        my_obj: string to convert
-        to json format
-    """
+def to_json_string(my_obj):
     return json.dumps(my_obj)
