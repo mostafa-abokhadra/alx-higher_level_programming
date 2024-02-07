6d3de37c0ff6c0445dceb2053e12433551a36575
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 17:54:29 +0200

    done

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""class to json module"""
 import json
 
+
 def class_to_json(obj):
+    """class to json
+
+    args:
+        obj: object to convert
+    """
     return json.dumps(obj.__dict__)
