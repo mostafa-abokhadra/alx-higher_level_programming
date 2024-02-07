eab6bc55d6e413935ec57cde3485884b298896c0
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 15:36:14 +0200

    done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,4 +11,4 @@ def save_to_json_file(my_obj, filename):
         filename: name of file
     """
     with open(filename, mode='w', encoding='utf-8') as fily:
-        filename.write(json.dumps(my_obj))
+        fily.write(json.dumps(my_obj))
