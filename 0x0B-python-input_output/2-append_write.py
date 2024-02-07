f0f8f634c74c3562b6a372c2be84b36b399539e0
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 15:16:14 +0200

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""append module"""
+
 
 def append_write(filename="", text=""):
+    """appending to a file
+
+    args:
+        filename: the file to append to
+        text: text to append to the file
+    """
     with open(filename, mode='a', encoding='utf-8') as fily:
         return fily.write(text)
