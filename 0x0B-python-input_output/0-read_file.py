d2c88d2e8e0d0e75f75ebc5456229701e5d83a53
mostafa bukhadra <mvxizk@gmail.com>
2024-02-07 15:11:21 +0200

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""reading from file"""
 
 
 def read_file(filename=""):
+    """reading a file
+
+    args:
+        filename: the file to be read
+    """
     with open(filename, mode='r', encoding='utf-8') as fily:
         print(fily.read(), end='')
