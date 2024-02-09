78a651c8d9a647b38a5886019b7c9c2d8b031119
mostafa bukhadra <mvxizk@gmail.com>
2024-02-09 17:18:50 +0200

    ok

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+
 
 class Base:
     __nb_objects = 0
@@ -8,3 +10,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
