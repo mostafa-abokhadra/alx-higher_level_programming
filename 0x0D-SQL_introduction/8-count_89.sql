a007535705ddc816e26dc1ad4577b844e16f6fb5
mostafa-abokhadra <mvxizk@gmail.com>
2024-03-16 20:56:51 +0200

    done

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Write a script that displays the number of records with id = 89 in the table first_table
+-- displays the number of records with id = 89 in the table first_table
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
