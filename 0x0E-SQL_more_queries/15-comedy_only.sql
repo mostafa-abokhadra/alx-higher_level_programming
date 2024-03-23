5268fb651ea0f8a6320c33d8ac96187e23dc5238
mostafa-abokhadra <mvxizk@gmail.com>
2024-03-23 17:57:33 +0200

    done

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,35 +1,6 @@
---  script that lists all Comedy shows in the database hbtn_0d_tvshows.
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 -- The database name will be passed as an argument of the mysql command
 
--- first solution
-/*
-SLECT tv_shows.title FROM tv_shows
-inner join tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-where tv_show_genres.genre_id = 
-(slect tv_genres.id from tv_genres where tv_genres.name = 'Comedy');
-order by title;
-*/
-
--- another method
-/*
-SLECT tv_shows.title FROM tv_shows
-inner join tv_show_genres inner join tv_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = tv_genres.id and tv_genres.name = 'Comedy';
-order by tv_shows.title;
-*/
-
--- another method
-/*
-SLECT tv_shows.title FROM tv_shows
-inner join tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-inner join tv_genres
-WHERE tv_show_genres.genre_id = tv_genres.id and tv_genres.name = 'Comedy';
-order by title;
-*/
-
-
--- the best solution
 SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
