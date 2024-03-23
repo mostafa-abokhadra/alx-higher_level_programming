ab4de434c8c63c5ee98d40dc365e3fd5a7135bd0
mostafa-abokhadra <mvxizk@gmail.com>
2024-03-23 15:35:39 +0200

    done

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres INNER JOIN tv_shows
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = tv_shows.id and tv_shows.title = 'Dexter';
