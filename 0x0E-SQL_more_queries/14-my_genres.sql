db7478388b2621a50a8ae3fe21a4cd7167d279da
mostafa-abokhadra <mvxizk@gmail.com>
2024-03-23 15:41:35 +0200

    done

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,6 +7,7 @@ ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id = tv_shows.id and tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
 */
+-- above code works but this is better and more readable:
 SELECT tv_genres.name FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
