95fd8d5afc89526c5005693d02006ad646d173ee
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-11 22:17:12 +0200

    ok

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const listy = [];
-  for (let i = 0; list[i]; i++) {
+  for (let i = 0; i < list.length; i++) {
     listy[i] = list[i];
   }
   return listy;
