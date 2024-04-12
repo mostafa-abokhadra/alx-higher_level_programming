a558d7c3a169634d17a5984ef90a0b2600a9d82d
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-12 20:06:45 +0200

    ok

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,18 +4,22 @@
 import sys
 import MySQLdb
 
-try:
-    connection = MySQLdb.connect
-    ('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
-except:
-    print("can't open\n")
 
-curse = connection.cursor()
-curse.execute('select * from states order by states.id')
-arr = curse.fetchall()
+def sql_connect():
+    """connect to sql server
+    """
+    try:
+        connection = MySQLdb.connect(
+                'localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    except Exception as err:
+        raise err("can't connect to sql server")
+        return 0
+    curse = connection.cursor()
+    curse.execute('select * from states order by states.id')
+    arr = curse.fetchall()
+    for data in arr:
+        print(data)
 
-for data in arr:
-    print(data)
 
 if __name__ == '__main__':
-    MySQLdb.main()
+    sql_connect()
