25dbd096551576123b7795870e8b131109d09a81
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-12 19:46:30 +0200

    ok

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,13 @@
 import sys
 import MySQLdb
 
-try:
-    connection = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
-except:
-    print("can't connect to database")
 
-curse = connection.cursor();
+connection = MySQLdb.connect
+('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+
+curse = connection.cursor()
 curse.execute('select * from states order by states.id')
 arr = curse.fetchall()
 
 for data in arr:
     print(data)
-
