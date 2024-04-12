e290a915906021a5675276c6091b37065a097dfc
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-12 20:28:10 +0200

    ok

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""list all states acc to names
+"""
+
+import sys
+import MySQLdb
+
+
+def connect_sql():
+    """connect to sql, some code
+    """
+    try:
+        connection = MySQLdb.connect(
+                'localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    except Exception as error:
+        raise error("can't connect to sql server")
+
+    cur = connection.cursor()
+    cur.execute("select * from states")
+    arr = cur.fetchall()
+    for data in arr:
+        if (data[1] == sys.argv[4]):
+            print(data)
+
+
+if __name__ == '__main__':
+    connect_sql()
