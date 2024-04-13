66ba0851e9e02aefe80e74a7ee9a92c5be3ba0ba
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-13 19:34:23 +0200

    ok

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,15 +10,13 @@ from sys import argv
 
 def alchemist_connection():
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(State).order_by(State.id).all()
-
-    for data in results:
-        print(data.id, ": ", data.name)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
     session.close()
 
 
