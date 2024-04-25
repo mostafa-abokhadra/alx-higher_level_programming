a5a0aa56a73427edbd9aa610383d9d5a710591b1
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-25 22:21:06 +0200

    ok

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # takes url arg, send request, and display size of response
-curl -i $1 | grep Accept-Ranges: >> file.txt
-cat file.txt
+curl -i $1 | grep Content-Length: | cut -b 16-
