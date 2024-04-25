274fd5078e2e6b7f4aa00ca173ab32a8ec66b0b7
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-25 23:04:07 +0200

    ok

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # takes in url, send get request, display if OK
-curl -i $1 >> file.txt
-line=$(head -n 1 file.txt)
-state=$(echo "$line" | cut -d " " -f2)
-
-echo "===================="
-if [ $state -eq "200" ]; then
-	cat file.txt
-fi
+curl -Ls "$1"
