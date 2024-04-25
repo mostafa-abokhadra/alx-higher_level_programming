8c31f9b9d33375fcfbe087d17337014e1f76e874
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-25 22:22:38 +0200

    ok

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes url arg, send request, and display size of response
-curl -i $1 | grep Content-Length: | cut -b 16-
+curl -si $1 | grep Content-Length: | cut -b 16-
