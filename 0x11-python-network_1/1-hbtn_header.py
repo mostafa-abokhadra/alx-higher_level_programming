234482e79d12a648bec2db9db2146bfbd7161e6d
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-26 21:39:35 +0300

    ok

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""send, receive and display"""
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    from sys import argv
+    req = Request(argv[1])
+    with urlopen(req) as response:
+        response.getheader('X-Request-Id')
