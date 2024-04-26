1f087419c38aaa21afcc90b37cd298713bcbea8d
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-26 21:16:14 +0300

    ok

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """fetching a url"""
-from urllib.request import Request, urlopen
-req = Request('https://alx-intranet.hbtn.io/status')
-with urlopen(req) as response:
-    page = response.read()
-    print("Body response:")
-    print("\t- {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode("utf-8")))
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    req = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(req) as response:
+        page = response.read()
+        print("Body response:")
+        print("\t- {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
