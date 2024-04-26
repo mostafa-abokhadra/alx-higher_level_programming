b576fdc48fd2cfc29ba2ab278c6de19bc1faa306
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-26 22:34:59 +0300

    ok

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     try:
         with request.urlopen(argv[1]) as response:
             body = response.read()
-            print(body.decode())
+            print(body.decode("utf-8"))
     except URLError as err:
         if hasattr(err, 'code'):
             print("Error code: {}".format(err.code))
