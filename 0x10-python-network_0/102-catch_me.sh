ed684c407d23d37b27296b61dc429ee1542ed29e
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-26 03:13:37 +0300

    ok

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # trying cool stuff
-curl -X POST -s 0.0.0.0:5000/catch_me --data "user_id=1000" -H "Content-Type: application/x-www-form-urlencoded" --location
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
