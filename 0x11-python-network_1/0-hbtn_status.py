fc0f7f20077697db337f613963f1e42906a274f7
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-26 21:17:12 +0300

    ok

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urlopen(req) as response:
         page = response.read()
         print("Body response:")
-        print("\t- {}".format(type(page)))
+        print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
         print("\t- utf8 content: {}".format(page.decode("utf-8")))
