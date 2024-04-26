dddd680780df643e1b325a656ba7f89e84adb4da
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 02:22:58 +0300

    ok

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""send, display"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
