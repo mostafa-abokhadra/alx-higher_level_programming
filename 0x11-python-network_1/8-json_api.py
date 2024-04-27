394b98ecaa9a1ab57cb7e591ef6bbd7226a8f576
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 03:23:37 +0300

    ok

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
             print("No result")
         else:
             print("[{}] {}".format(dic.get("id"), dic.get("name")))
-    except Exception:
+    except ValueError:
         print("Not a valid JSON")
