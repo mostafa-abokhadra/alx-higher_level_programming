90771e8d60bd1aab2fe946f7ce49dbcb21324196
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 03:33:17 +0300

    ok

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,11 +5,12 @@
 if __name__ == '__main__':
     import requests
     from sys import argv
+    url = "http://0.0.0.0:5000/search_user"
     letter = "" if len(argv) == 1 else argv[1]
     datum = {"q": letter}
 
     dic = datum
-    res = requests.post("http://0.0.0.0:5000/search_user", data=dic)
+    res = requests.post(url, data=dic)
     try:
         dic = res.json()
         """if dic == {}:
