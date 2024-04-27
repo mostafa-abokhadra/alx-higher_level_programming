21d3d6a8f4888843ee422e9a358be5ddc7a282f2
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 05:14:10 +0300

    ok

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""listing commits"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    from requests.auth import HTTPBasicAuth
+
+    res = requests.get("https://developer.github.com/v3/repos/{}/{}/commits/".format(argv[1], argv[2]))
+    commits = res.json()
+    for commit in commits:
+        print(commit['sha'],": ", commit['commit']['committer']['name'])
