2691e5a755d1066ab3374abdf24e49412aa9d3c7
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 03:03:40 +0300

    ok

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""json api"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = "http://0.0.0.0:5000/search_user"
+    dic = {}
+    if len(argv) == 2:
+        dic["q"] = argv[1]
+    else:
+        dic["q"] = ""
+    try:
+        res = requests.post(url, data=dic)
+        dic = res.json()
+        if not len(dic) == 0:
+            for key, value in dic.items():
+                print("[{}] {}".format(id(key), value))
+        else:
+            print("No result")
+    except Exception as error:
+        print("Not a valid JSON")
