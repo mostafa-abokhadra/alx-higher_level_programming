286b8697e131eed107f4c6e0f96d67c834064569
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 05:22:06 +0300

    ok

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     from sys import argv
     from requests.auth import HTTPBasicAuth
 
-    res = requests.get("https://developer.github.com/v3/repos/{}/{}/commits/".format(argv[1], argv[2]))
+    res = requests.get("https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]))
     commits = res.json()
-    for commit in commits:
-        print(commit['sha'],": ", commit['commit']['committer']['name'])
+    for i in range(10):
+        print("{}: {}".format(
+            commits[i].get("sha"),
+            commits[i].get("commit").get("author").get("name")))
