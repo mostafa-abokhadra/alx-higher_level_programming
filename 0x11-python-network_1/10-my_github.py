494b05ae53c4e70ec4fb1bf5f03ce1fc407890b6
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-27 04:19:36 +0300

    ok

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,6 @@ if __name__ == '__main__':
     from sys import argv
     from requests.auth import HTTPBasicAuth
 
-    url = "https://api.github.com/user"
     authentecation = HTTPBasicAuth(argv[1], argv[2])
-    res = requests.get(url, auth=authentecation)
+    res = requests.get("https://api.github.com/user", auth=authentecation)
     print(res.json().get("id"))
