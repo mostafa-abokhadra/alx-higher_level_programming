1a187d3694934693cc32d84f9cad388e6c93062b
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-14 20:01:56 +0300

    ok

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
-process.stdout.write(proccess.argv[1])
+#!/usr/bin/node
+/*reading file*/
+const fs = require('node:fs')
+
+try {
+	const data = fs.readFileSync(process.argv[2], 'utf-8')
+	console.log(data)
+}
+catch (err) {
+	console.log(err)
+}
