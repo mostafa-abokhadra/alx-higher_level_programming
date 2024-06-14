46335c8bca711eb48556a7fd2f6bf17265862ed9
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-15 01:08:34 +0300

    ok

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// no of movies actress is present
+
+const request = require('request');
+let str = '';
+for (let i = process.argv[2].length; i >= 0; i--) {
+  if (process.argv[2][i] === '/') {
+    for (let j = 0; j < i; j++) {
+      str = str + process.argv[2][j];
+    }
+    str += '/people/18';
+    break;
+  }
+}
+request(str, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).films.length);
+  }
+});
