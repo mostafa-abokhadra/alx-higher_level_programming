8678064de4e5fa369f6fe3d14b1e92980569be0d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-15 03:31:34 +0300

    ok

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,8 @@
-/*#!/usr/bin/node
-// completed task
-
-const request = require('request');
-request(process.argv[2], (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-  const users = {};
-  const myjson = JSON.parse(body);
-  for (let i = 0; i < myjson.length; i++) {
-    if (myjson[i].userId in users === false) {
-      users[myjson[i].userId] = 0;
-    }
-    if (myjson[i].completed) {
-      users[myjson[i].userId] += 1;
-    }
-  }
-  console.log(users);
-});
-*/
 #!/usr/bin/node
 
 const request = require('request');
 
-request(process.argv[2], function (err, _res, body) {
+request(process.argv[2], (err, _res, body) => {
   if (err) {
     console.log(err);
   } else {
