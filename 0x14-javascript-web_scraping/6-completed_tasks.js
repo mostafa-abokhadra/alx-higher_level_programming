bbed90d13dd75d80184c4107db65d526c88827fe
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-15 03:27:25 +0300

    ok

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+/*#!/usr/bin/node
 // completed task
 
 const request = require('request');
@@ -17,4 +17,30 @@ request(process.argv[2], (err, res, body) => {
     }
   }
   console.log(users);
+});
+*/
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
 });
